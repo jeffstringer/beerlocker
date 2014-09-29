2aa2c354a17d74212fd3b00bcb921125b8626099
jeffstringer <jeff.j.stringer@gmail.com>
2014-09-29 13:19:57 -0700

    add remaining crud functionality and css

---
+++ routes/index.js
@@ -0,0 +1,73 @@
+var mongoose = require('mongoose');
+var Beer = mongoose.model('Beer');
+
+exports.index = function (req, res) {
+  Beer.
+    find().
+    exec( function (err, beers) {
+      res.render('index', {
+        title: 'Beer Locker',
+        beers: beers
+      });
+    });
+};
+
+exports.create = function(req, res) {
+  var beer = new Beer();
+  beer.name = req.body.name;
+  beer.type = req.body.type;
+  beer.quantity = req.body.quantity;
+  beer.save(function(err){
+    if(err) {
+      res.send(err);
+    }
+    res.redirect('/');
+  });
+};
+
+exports.update = function(req, res) {
+  Beer.findById(req.params.id, function(err, beer) {
+    if (err) {
+      res.send(err);
+    }
+    beer.name = req.body.name;
+    beer.type = req.body.type;
+    beer.quantity = req.body.quantity;
+    beer.save(function(err) {
+      if(err) {
+        res.send(err);
+      }
+    res.redirect('/');
+    });
+  });
+};
+
+exports.edit = function(req, res) {
+  Beer.
+    findById(req.params.id).
+    exec(function(err, beer) {
+    res.render('edit', {
+      title: "Update " + beer.name,
+      beer: beer,
+    });
+  });
+};
+
+exports.beer = function(req, res) {
+  Beer.
+    findById(req.params.id).
+    exec(function(err, beer) {
+      res.render('beer', {
+        title: "More about yer beer:",
+        beer: beer
+      });
+    });
+};
+
+exports.destroy = function(req, res) {
+  Beer.findByIdAndRemove(req.params.id, function(err, beer) {
+    if (err)
+      res.send(err);
+    res.redirect('/');
+  });
+};
--- server.js
+++ server.js
@@ -3,78 +3,29 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var Beer = require('./models/beer');
 var port = process.env.PORT || 3000;
+var routes  = require( './routes' );
 var router = express.Router();
+var http = require('http');
+var path = require('path');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(express.static(path.join(__dirname, '/public')));
+app.set( 'view engine', 'hjs' );
+
 mongoose.connect('mongodb://localhost:27017/beerlocker');
 
-app.use('/api', router);
+app.use('/', router);
+app.get('/', routes.index);
+app.post('/create', routes.create);
+app.get('/edit/:id', routes.edit);
+app.post('/update/:id', routes.update);
+app.get('/beer/:id', routes.beer);
+app.get('/destroy/:id', routes.destroy);
 
 app.listen(port);
 console.log('Insert beer on port ' + port);
-
-router.get('/', function(req, res) {
-  res.json({message: 'You are running dangerously low on beer!'});
-});
-
-var beersRoute = router.route('/beers');
-
-beersRoute.post(function(req, res) {
-  var beer = new Beer();
-  beer.name = req.body.name;
-  beer.type = req.body.type;
-  beer.quantity = req.body.quantity;
-  beer.save(function(err){
-    if(err) {
-      res.send(err);
-    }
-    res.json({message: 'Beer added to the locker!', data: beer});
-  });
-});
-
-beersRoute.get(function(req, res) {
-  Beer.find(function(err, beers) {
-    if (err) {
-      res.send(err);
-    }
-    res.json(beers);
-  });
-});
-
-var beerRoute = router.route('/beers/:beer_id');
-
-beerRoute.get(function(req, res) {
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if (err) {
-      res.send(err);
-    }
-    res.json(beer);
-  });
-});
-
-beerRoute.put(function(req, res) {
-  Beer.findById(req.params.beer_id, function(err, beer) {
-    if (err) {
-      res.send(err);
-    }
-    beer.quantity = req.body.quantity;
-    beer.save(function(err) {
-      if(err) {
-        res.send(err);
-      }
-    res.json(beer);
-    });
-  });
-});
-
-beerRoute.delete(function(req, res) {
-  Beer.findByIdAndRemove(req.params.beer_id, function(err, beer) {
-    if (err)
-      res.send(err);
-    res.json({message: 'Beer removed from the locker'});
-  });
-});
